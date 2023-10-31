98bc3b970b9e56b9fb8f1ef5eb01ff399aac7f3f
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2023-10-31 17:21:40 +0200

    Edit: use format function instead 'f' specifier in 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,5 +3,5 @@ toUpper = 0
 for char in range(122, 96, -1):
     if (char % 2 != 0):
         toUpper = 32
-    print(f"{chr(char - toUpper)}", end="")
+    print("{}".format(chr(char - toUpper)), end="")
     toUpper = 0
