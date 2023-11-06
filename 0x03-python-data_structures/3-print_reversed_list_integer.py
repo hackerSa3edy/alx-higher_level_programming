038ce2005e4ae0b3d637a1403d08a01ad1f01147
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2023-11-06 08:37:46 +0200

    Fix bug in the code and pycodestyle

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
-def print_reversed_list_integer(my_list = []):
-    my_list.reverse()
-    for n in my_list:
+
+def print_reversed_list_integer(my_list=[]):
+    new_list = my_list.copy()
+    new_list.reverse()
+    for n in new_list:
         print("{:d}".format(n))
