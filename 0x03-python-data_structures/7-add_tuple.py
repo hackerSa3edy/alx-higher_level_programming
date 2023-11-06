fc35493a4146855aa10e0b8e2cde74dccfa8f916
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2023-11-06 14:23:00 +0200

    Add 7-add_tuple.py

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    if not (isinstance(tuple_a, tuple) and isinstance(tuple_b, tuple)):
+        return
+
+    t_a_len = len(tuple_a)
+    t_b_len = len(tuple_b)
+
+    if t_a_len < 2:
+        tuple_a = tuple_a + (0, 0)
+
+    if t_b_len < 2:
+        tuple_b = tuple_b + (0, 0)
+
+
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+
+
+
