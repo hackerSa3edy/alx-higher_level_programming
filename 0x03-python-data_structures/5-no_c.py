189e076f698b7fdaea5a5c0124684acf19c55901
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2023-11-06 08:59:29 +0200

    Replace abandont function

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    my_string = my_string.replace('c', '')
-    my_string = my_string.replace('C', '')
+    my_string = ''.join(my_string.split('c'))
+    my_string = ''.join(my_string.split('C'))
     return my_string
