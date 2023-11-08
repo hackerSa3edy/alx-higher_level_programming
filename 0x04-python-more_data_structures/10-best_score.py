57930d3c05aa9f8045a28c34cb80f8054f48cf0e
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2023-11-08 21:38:54 +0200

    Add 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if not isinstance(a_dictionary, dict):
+        return None
+
+    big_num = None
+    big_num_key = None
+    for key in a_dictionary:
+        if big_num is None:
+            big_num = a_dictionary[key]
+            continue
+
+        if a_dictionary[key] > big_num:
+            big_num = a_dictionary[key]
+            big_num_key = key
+
+    return big_num_key
