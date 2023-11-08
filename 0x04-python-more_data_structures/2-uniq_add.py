9d63f0887772da662ea02b644c3005297531a3ac
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2023-11-08 15:08:17 +0200

    Add 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+
+def uniq_add(my_list=[]):
+    if not isinstance(my_list, list):
+        return my_list
+
+    summition = 0
+    for element in set(my_list):
+        summition += element
+
+    return summition
