41119080d853423ab6d9f0c5bafe533d49bba3ab
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2023-11-08 14:55:14 +0200

    Add 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+
+def search_replace(my_list, search, replace):
+    if not isinstance(my_list, list):
+        return my_list
+    new_list = my_list.copy()
+    for element in range(len(new_list)):
+        if new_list[element] == search:
+            new_list[element] = replace
+
+    return new_list
