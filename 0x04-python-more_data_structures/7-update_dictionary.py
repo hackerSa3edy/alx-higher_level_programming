163e73188d68b61f7c1bc04e786f254ffd69adad
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2023-11-08 15:43:35 +0200

    Add 7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+
+
+def update_dictionary(a_dictionary, key, value):
+    if isinstance(a_dictionary, dict):
+        a_dictionary.update({key: value})
+        return a_dictionary
