e72a8ac1dd3f1ceb68b22e9221f1b4c49125bc30
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2023-11-09 00:47:36 +0200

    Prevent using abandoned instructions

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return [list(map(lambda x: x ** 2, item)) for item in matrix]
+    return list(map(lambda item: list(map(lambda x: x ** 2, item)), matrix))
