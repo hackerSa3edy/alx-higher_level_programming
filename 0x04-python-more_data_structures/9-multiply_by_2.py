1c9d08cfd1823add3090e0945b0e2eb1edce4f45
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2023-11-08 21:13:28 +0200

    Add 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+def multiply_by_2(a_dictionary):
+    if isinstance(a_dictionary, dict):
+
+        def mul_func(key, value):
+            return {key: value * 2}
+
+        new_dict = {}
+        for item in map(mul_func, a_dictionary.keys(), a_dictionary.values()):
+            new_dict.update(item)
+
+        return new_dict
