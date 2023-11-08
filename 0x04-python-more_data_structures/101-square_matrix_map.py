a2b544c3f21eefb47d6ed4f9dd346088a118c05d
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2023-11-09 00:39:19 +0200

    Shorten the code

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-
 def square_matrix_map(matrix=[]):
-    square_matrix = [list(map(lambda x: x ** 2, item)) for item in matrix]
-    return square_matrix
+    return [list(map(lambda x: x ** 2, item)) for item in matrix]
