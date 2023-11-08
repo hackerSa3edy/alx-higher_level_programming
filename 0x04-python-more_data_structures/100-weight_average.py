5edab2a6de77c44347cf946becc16c478028f697
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2023-11-09 00:26:28 +0200

    Add 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    average = 0
+    numerator = 0
+    denominator = 0
+    for item in my_list:
+        numerator += (item[0] * item[1])
+        denominator += item[1]
+    else:
+        average = numerator / denominator
+
+    return average
