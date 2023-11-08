1a2d0bb059b6d564dbd21f1726988ebb65ace8b6
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2023-11-09 00:57:10 +0200

    102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+
+def complex_delete(a_dictionary, value):
+    temp_dict = a_dictionary.copy()
+    for key, val in temp_dict.items():
+        if value == val:
+            a_dictionary.pop(key)
+
+    return a_dictionary
