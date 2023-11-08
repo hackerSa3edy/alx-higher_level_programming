afdd9b682de0b49d1a45d0f42d1831d40e2bd55f
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2023-11-08 15:54:12 +0200

    Add 8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+
+
+def simple_delete(a_dictionary, keys=""):
+    if isinstance(a_dictionary, dict):
+        if a_dictionary.get(keys):
+            a_dictionary.pop(keys)
+
+        return a_dictionary
