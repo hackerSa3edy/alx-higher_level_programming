352e6aad493d92b5ac7afbeeab37f315e10d4b1d
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2023-11-21 17:55:58 +0200

    Add advanced tasks

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+import sys
+
 
 def safe_function(fct, *args):
-
+    result = None
+    try:
+        result = fct(*args)
+    except Exception as err:
+        sys.stderr.write("Exception: {}\n".format(err))
+    finally:
+        return result
