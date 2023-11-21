314bf4f2efd5a3f25e3d00bbde977cc182642080
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2023-11-21 18:00:01 +0200

    Replace BINARY_ADD with INPLACE_ADD

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,7 +7,7 @@ def magic_calculation(a, b):
             if i > a:
                 raise Exception('Too far')
             else:
-                result = ((a ** b) / i) + result
+                result += ((a ** b) / i)
         finally:
             result = b + a
             break
