fd3b777a6ccc55327de3005ab3981b471a024622
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2023-11-21 18:26:03 +0200

    Rename main.py -> 102-main.py

--- 0x05-python-exceptions/102-main.py
+++ 0x05-python-exceptions/102-main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 magic_calculation = __import__('102-magic_calculation')
 try:
-    print(magic_calculation.magic_calculation(4, 1))
+    print(magic_calculation.magic_calculation(3, 3))
 except Exception as e:
     print("Exception: {}".format(e))
