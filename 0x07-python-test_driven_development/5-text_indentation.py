27d1c970f8d7265aa6259680242112273d0cf5f4
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2023-12-03 01:57:57 +0200

    Add task 5-text_indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+5-text_indentation module
+
+Inserts 2 new lines after each occurance of one of those
+characters [. ? :]
+"""
+
+
+def text_indentation(text):
+    """text_indentation
+
+    Arguments:
+        text -- The text to be processed
+
+    Raises:
+        TypeError: text must be a string
+    """
+    if not isinstance(text, str):
+        raise TypeError('text must be a string')
+
+    new_text = text.replace('.', '.\n\n')
+    new_text = new_text.replace('?', '?\n\n')
+    new_text = new_text.replace(':', ':\n\n')
+
+    print(new_text)
