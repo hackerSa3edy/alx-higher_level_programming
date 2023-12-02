dab1a6e8c8443bcb2f759c1390bd96f4e4bf9ef0
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2023-12-02 23:39:01 +0200

    Add task files

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+add_integer module
+
+Adds two numbers whether float or int and return an integer
+summation of them.
+"""
+
+def add_integer(a, b=98):
+    """add_integer
+
+    Arguments:
+        a -- number one
+
+    Keyword Arguments:
+        b -- number two (default: {98})
+
+    Raises:
+        TypeError: a must be an integer
+        TypeError: b must be an integer
+
+    Returns:
+        Summation of the two numbers.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+
+    return (a + b)
