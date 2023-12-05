0407913de4a8c7eb707c988297b3fead0bf11348
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2023-12-05 23:15:24 +0200

    Handle FileNotFoundError exception

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,17 +5,17 @@
 Script that adds all arguments to a Python list, and then
 save them to a file.
 """
-import json
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-sysArgv = sys.argv[1:]
 filename = "add_item.json"
 loaded_json = []
 
-
-json_data = load_from_json_file(filename)
-loaded_json.append(json_data)
-loaded_json.append(sysArgv)
+try:
+    json_data = load_from_json_file(filename)
+    loaded_json.extend(json_data)
+except FileNotFoundError:
+    pass
+loaded_json.extend(sys.argv[1:])
 save_to_json_file(loaded_json, filename)
