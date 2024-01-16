a4b792f593d31679e19e4fcfaadc90a5c01f8aa4
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2024-01-16 22:48:06 +0200

    [task100] Edit the query to convert the table to utf8 character set

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -7,4 +7,4 @@ USE hbtn_0c_0;
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- Change table character set
-ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
