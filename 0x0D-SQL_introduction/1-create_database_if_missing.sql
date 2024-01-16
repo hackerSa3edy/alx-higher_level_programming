a1b4e83a2416cd399d759987f93dfb41e3e953e9
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2024-01-16 20:11:05 +0200

    [0-9_TASKS] Add the solution for mandatory tasks

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,2 @@
+-- Creates the databse hbtn_0c_0 if not exists
+CREATE database IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,15 @@
+-- Creates a table second_table and add multiples rows
+
+-- Create the second_table if not exists
+CREATE TABLE IF NOT EXISTS second_table(
+	id INT,
+	name VARCHAR(256),
+	score INT
+	);
+
+-- Add multiple rows in second_table
+INSERT INTO second_table(id, name, score) VALUES
+	(1, 'John', 10),
+	(2, 'Alex', 3),
+	(3, 'Bob', 14),
+	(4, 'George', 8);
