f5b3e1f3b871d0353f2ee9bf7fe0294209ff826e
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2024-01-16 22:45:05 +0200

    [100-103_TASKS] Add the solution for the advanced tasks

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,10 @@
+-- Converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
+
+-- Using hbtn_0c_0 database
+USE hbtn_0c_0;
+
+-- Change database character set
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Change table character set
+ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
