d3dfa353a6458f6a848ec13d5ffe78374f44ecd7
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2024-01-18 00:12:07 +0200

    [6-7_TASKS] Use the database after creating it.

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -3,6 +3,7 @@
 -- Creates the database hbtn_0d_usa
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+USE hbtn_0d_usa;
 -- Creates the table states
 CREATE TABLE IF NOT EXISTS states(
 	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,6 +3,7 @@
 -- Creates the database hbtn_0d_usa
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+USE hbtn_0d_usa;
 -- Creates the table cities
 CREATE TABLE IF NOT EXISTS cities (
 	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
