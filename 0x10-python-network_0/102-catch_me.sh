824340069d3156f3b1a65965d093fc62492a3ed3
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2024-02-29 22:35:37 +0200

    Fix curl command in 101-post_json.sh and 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  Makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
-curl -X PUT -F 'user_id=98' --url '0.0.0.0:5000/catch_me' -L -H 'Origin: School'
+curl -sL -X PUT -d 'user_id=98' --url '0.0.0.0:5000/catch_me' -H 'Origin: School'
