3742e2a94e9f06d29ebafd54113b72e95d241f7a
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2024-02-29 22:24:29 +0200

    [TASK101-102] Update POST request URL and add quotes to URL in catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  Makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
-curl -X PUT -F 'user_id=98' --url localhost:5050/catch_me -L -H 'Origin: School'
+curl -X PUT -F 'user_id=98' --url '0.0.0.0:5000/catch_me' -L -H 'Origin: School'
