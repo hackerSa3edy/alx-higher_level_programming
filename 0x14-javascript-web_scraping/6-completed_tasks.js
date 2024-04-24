55e3d573788c7b20920c76d8b84a8b54cf200c82
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2024-04-24 05:56:39 +0200

    Refactor JavaScript web scraping script to use request.get for making HTTP requests and count the number of completed tasks for each user

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,40 @@
 #!/usr/bin/node
+/**
+ * This Node.js script makes a GET request to a specified URL and counts the number of completed tasks for each user.
+ *
+ * It uses the 'request' module to make HTTP requests.
+ *
+ * The script expects the API URL as a command line argument (process.argv[2]).
+ *
+ * The 'request.get' function is used to make a GET request. It takes two arguments:
+ * 1. The URL
+ * 2. A callback function that is called when the HTTP request is complete.
+ *
+ * The callback function parses the response body as JSON, iterates over the todos, and maintains a count of completed tasks for each user in the 'allData' object.
+ *
+ * If a todo is not completed, it is skipped. If a todo is completed, the count for the user is incremented. If the user does not exist in 'allData', they are added with a count of 1.
+ *
+ * Finally, it logs the 'allData' object to the console.
+ */
 const request = require('request');
-request(process.argv[2], function (error, response, body) {
-  if (!error) {
-    const todos = JSON.parse(body);
-    let completed = {};
-    todos.forEach((todo) => {
-      if (todo.completed && completed[todo.userId] === undefined) {
-        completed[todo.userId] = 1;
-      } else if (todo.completed) {
-        completed[todo.userId] += 1;
-      }
-    });
-    console.log(completed);
-  }
+const url = process.argv[2];
+
+request.get(url, (_, resp, body) => {
+  const allData = {};
+  const reqBody = JSON.parse(body);
+
+  reqBody.forEach(todo => {
+    if (todo.completed !== true) {
+      return;
+    }
+
+    if (todo.userId in allData) {
+      allData[`${todo.userId}`] = allData[`${todo.userId}`] + 1;
+      return;
+    }
+
+    allData[`${todo.userId}`] = 1;
+  });
+
+  console.log(allData);
 });
