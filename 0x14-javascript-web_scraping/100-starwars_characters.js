9886794cf916b9774a88e0b7a69ef1025cf87719
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2024-04-24 06:33:09 +0200

    Refactor JavaScript web scraping script to use request.get for making HTTP requests and log the name of each character in a specific film

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,15 +1,34 @@
 #!/usr/bin/node
-const request = require("request");
-const url = "https://swapi-api.hbtn.io/api/films/" + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (!error) {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    });
-  }
+/**
+ * This Node.js script fetches data from the Star Wars API (SWAPI) and logs the name of each character in a specific film.
+ *
+ * It uses the 'request' module to make HTTP requests.
+ *
+ * The script expects the film ID as a command line argument (process.argv[2]).
+ *
+ * The 'request.get' function is used to make a GET request to the SWAPI for the film data. It takes two arguments:
+ * 1. The URL, which includes the film ID
+ * 2. A callback function that is called when the HTTP request is complete.
+ *
+ * The callback function parses the response body as JSON, iterates over the characters, and calls the 'logCharacterName' function for each character URL.
+ *
+ * The 'logCharacterName' function makes another GET request to the SWAPI for the character data and logs the character's name to the console.
+ */
+const request = require('request');
+
+const movieID = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieID}/`;
+
+request.get(url, (_, resp, body) => {
+  const jsonBody = JSON.parse(body);
+
+  jsonBody.characters.forEach(url => {
+    logCharacterName(url);
+  });
 });
+
+function logCharacterName (url) {
+  request.get(url, (_, resp, body) => {
+    console.log(JSON.parse(body).name);
+  });
+}
