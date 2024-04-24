5f9f44a888a4a43fbffe52a833b053f929926abc
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2024-04-24 07:45:05 +0200

    Refactor JavaScript web scraping script to use request.get for making HTTP requests and log the name of each character in a specific film

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,15 +1,36 @@
 #!/usr/bin/node
-const request = require("request");
-const url = "https://swapi-api.hbtn.io/api/films/" + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (!error) {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    });
-  }
+/**
+ * This Node.js script fetches data from the Star Wars API (SWAPI) and logs the name of each character in a specific film.
+ *
+ * It uses the 'request' module to make HTTP requests.
+ *
+ * The script expects the film ID as a command line argument (process.argv[2]).
+ *
+ * The 'request.get' function is used to make a GET request to the SWAPI for the film data. It takes two arguments:
+ * 1. The URL, which includes the film ID
+ * 2. A callback function that is called when the HTTP request is complete.
+ *
+ * The callback function parses the response body as JSON, and calls the 'logCharacterName' function with the characters' URLs and an initial index of 0.
+ *
+ * The 'logCharacterName' function makes another GET request to the SWAPI for the character data at the given index and logs the character's name to the console. It then increments the index and calls itself recursively if there are more characters to fetch.
+ */
+const request = require('request');
+
+const movieID = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieID}/`;
+
+request.get(url, (_, resp, body) => {
+  const jsonBody = JSON.parse(body);
+  logCharacterName(jsonBody.characters, 0);
 });
+
+function logCharacterName (urls, findex) {
+  request.get(urls[findex], (_, resp, body) => {
+    console.log(JSON.parse(body).name);
+
+    findex++;
+    if (findex + 1 <= urls.length) {
+      logCharacterName(urls, findex);
+    }
+  });
+}
