edd48351a3080b749a4590a3a40524f34f3fe290
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2024-05-08 09:50:40 +0300

    feat: Add jQuery script to display Star Wars character name
    This commit adds a new HTML file `7-main.html` and a new JavaScript file `7-script.js`. The HTML file includes a `<div>` element with the id `character`, which will be used to display the name of a Star Wars character. The JavaScript file contains a jQuery script that makes an AJAX GET request to the SWAPI API to fetch the data of a specific character (ID 5) and updates the text of the `character` element with the character's name.
    
    Based on the recent user commits, this commit message aligns with the purpose of the code changes and follows the established commit message conventions.

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $.ajax({
+    type: 'GET',
+    url: 'https://swapi-api.alx-tools.com/api/people/5/?format=json',
+    dataType: 'json',
+    success: function (response) {
+      const name = response.name;
+      $('div#character').text(name);
+    }
+  });
+});
